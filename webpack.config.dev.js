3d31a39e449050681ca10fd6f5bb9676e1c6c908
mineyan <arminegevorgyan1993@gmai.com>
2018-09-17 15:02:21 +0400

    webpack worked

---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+import path from 'path';
+
+export default {
+  entry: path.join(__dirname, '/client/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: '/' 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'client'),
+        loaders: [ 'babel-loader' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [  '.js' ]
+  },
+  mode: 'none',
+  plugins: [
+    
+  ]
+}
